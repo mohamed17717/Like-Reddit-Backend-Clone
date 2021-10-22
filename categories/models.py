c8cf215d2e1491dbc1e2c1ba72f381a5b14de188
Mo17 <mo.err17@gmail.com>
2021-10-22 23:12:25 +0200

    add new fields and rename others

--- categories/models.py
+++ categories/models.py
@@ -1,25 +1,33 @@
 from django.db import models
 
 
+# W: Admin | R: Anyone
 class Category(models.Model):
-  title = models.CharField(max_length=128)
+  name = models.CharField(max_length=32)
+
+  created = models.DateField(auto_now_add=True)
+  updated = models.DateField(auto_now=True)
 
   class Meta:
     verbose_name = 'Category'
     verbose_name_plural = 'Categories'
 
   def __str__(self):
-    return self.title
+    return self.name
 
 
+# W: Admin | R: Anyone
 class SubCategory(models.Model):
-  title = models.CharField(max_length=128)
+  name = models.CharField(max_length=32)
   category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='sub_categories')
+
+  created = models.DateField(auto_now_add=True)
+  updated = models.DateField(auto_now=True)
 
   class Meta:
     verbose_name = 'SubCategory'
     verbose_name_plural = 'SubCategories'
 
   def __str__(self):
-    return f'{self.title} ({self.category.title})'
+    return f'{self.name} ({self.category.name})'
 
