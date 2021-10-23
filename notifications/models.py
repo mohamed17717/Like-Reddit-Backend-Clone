ad9efb53952c831c376a793cc067b0ad1f804b1f
Mo17 <mo.err17@gmail.com>
2021-10-23 16:55:33 +0200

    create app and its models

---
+++ notifications/models.py
@@ -0,0 +1,68 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+
+User = get_user_model()
+
+# W: Static | R: Runtime
+class NotificationType(models.Model):
+  type = models.CharField(max_length=64)
+
+  class Meta:
+    verbose_name = 'NotificationType'
+    verbose_name_plural = 'NotificationTypes'
+
+  def __str__(self):
+    return self.type
+
+
+# W: Static | R: Runtime
+class NotificationMessage(models.Model):
+  type = models.ForeignKey(NotificationType, on_delete=models.CASCADE, related_name='messages')
+  message_format = models.TextField()
+
+  class Meta:
+    verbose_name = 'NotificationMessage'
+    verbose_name_plural = 'NotificationMessages'
+
+  def __str__(self):
+    return f'{self.message_format} ({self.type})'
+
+
+# W: Anyone (signals) | R: Anyone
+class Notification(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
+  type = models.ForeignKey(NotificationType, on_delete=models.CASCADE, related_name='notifications')
+
+  sender_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+  sender_id = models.PositiveIntegerField()
+  sender_object = GenericForeignKey('sender_type', 'sender_id')
+
+  created = models.DateField(auto_now_add=True)
+  updated = models.DateField(auto_now=True)
+
+  class Meta:
+    verbose_name = 'Notification'
+    verbose_name_plural = 'Notifications'
+
+  def __str__(self):
+    return f'{self.user} ({self.type})'
+
+
+# W: Runtime | R: Runtime
+class NotificationSender(models.Model):
+  notification = models.ForeignKey(Notification, on_delete=models.CASCADE, related_name='senders')
+
+  sender_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+  sender_id = models.PositiveIntegerField()
+  sender_object = GenericForeignKey('sender_type', 'sender_id')
+
+  class Meta:
+    verbose_name = 'NotificationSender'
+    verbose_name_plural = 'NotificationSenders'
+
+  def __str__(self):
+    return f'({self.notification.pk}) {self.sender_object}'
+
