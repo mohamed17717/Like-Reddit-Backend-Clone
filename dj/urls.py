94d03c121547b782cd4eba342e47ef98f9ce69ec
Mo17 <mo.err17@gmail.com>
2021-11-02 08:16:32 +0200

    add privates urls

--- dj/urls.py
+++ dj/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
   path('', include('categories.urls', namespace='categories')),
   path('', include('follows.urls', namespace='follows')),
   path('', include('notifications.urls', namespace='notifications')),
+  path('', include('privates.urls', namespace='privates')),
 
   path('admin/', admin.site.urls),
   path('api-auth/', include('rest_framework.urls'))
