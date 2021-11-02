ba3a6f367fe48b5373704962ed5b440b75f0e1bc
Mo17 <mo.err17@gmail.com>
2021-11-02 03:39:17 +0200

    register urls of all views (created till now)

--- dj/urls.py
+++ dj/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
+  path('', include('categories.urls', namespace='categories')),
+  path('', include('follows.urls', namespace='follows')),
+
   path('admin/', admin.site.urls),
   path('api-auth/', include('rest_framework.urls'))
 ]
