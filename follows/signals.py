24ade43f7be73915bf06e7d1d2032fc28de4df32
Mo17 <mo.err17@gmail.com>
2021-11-20 19:35:45 +0200

    replace signal from follows to threads which make more sense

--- follows/signals.py
+++ follows/signals.py
@@ -2,20 +2,7 @@ from django.db.models.expressions import F
 from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
 
-from follows.models import ThreadFollow
-
-# senders
-from threads.models import ThreadPost
 from follows.models import UserFollow
-
-
-@receiver(post_save, sender=ThreadPost)
-def user_start_follow_thread(sender, instance, created, **kwargs):
-  if created:
-    thread = instance.thread
-    user = instance.post.user
-
-    ThreadFollow.objects.get_or_create(target=thread, follower=user)
 
 
 @receiver(post_save, sender=UserFollow)
--- threads/signals.py
+++ threads/signals.py
@@ -3,7 +3,17 @@ from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
 
 from threads.models import Thread
+from threads.models import ThreadPost
+from follows.models import ThreadFollow
 
+
+@receiver(post_save, sender=ThreadPost)
+def user_start_follow_thread_whenever_commented(sender, instance, created, **kwargs):
+  if created:
+    thread = instance.thread
+    user = instance.post.user
+
+    ThreadFollow.objects.get_or_create(target=thread, follower=user)
 
 
 @receiver(post_save, sender=Thread)
