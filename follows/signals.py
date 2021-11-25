d23b33176298a2c8bafe8a3338cfcd97ca03573b
Mo17 <mo.err17@gmail.com>
2021-11-25 21:03:45 +0200

    solve update issue

--- follows/signals.py
+++ follows/signals.py
@@ -3,7 +3,7 @@ from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
 
 from follows.models import UserFollow
-
+from accounts.models import UserProfile
 
 @receiver(post_save, sender=UserFollow)
 def increase_follow_counter_in_profile(sender, instance, created, **kwargs):
@@ -11,15 +11,14 @@ def increase_follow_counter_in_profile(sender, instance, created, **kwargs):
     target = instance.target
     follower = instance.follower
 
-    follower.profile.update(following_count=F('following_count') + 1)
-    target.profile.update(follower_count=F('follower_count') + 1)
-
+    UserProfile.objects.select_for_update().filter(pk=target.pk).update(follower_count=F('follower_count') + 1)
+    UserProfile.objects.select_for_update().filter(pk=follower.pk).update(following_count=F('following_count') + 1)
 
 @receiver(pre_delete, sender=UserFollow)
 def decrease_follow_counter_in_profile(sender, instance, *args, **kwargs):
   target = instance.target
   follower = instance.follower
 
-  follower.profile.update(following_count=F('following_count') - 1)
-  target.profile.update(follower_count=F('follower_count') - 1)
+  UserProfile.objects.select_for_update().filter(pk=target.pk).update(follower_count=F('follower_count') - 1)
+  UserProfile.objects.select_for_update().filter(pk=follower.pk).update(following_count=F('following_count') - 1)
 
