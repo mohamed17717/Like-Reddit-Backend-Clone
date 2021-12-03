5a5b0b0df34562a3fa9a04bf76ef5391a2466bc8
Mo17 <mo.err17@gmail.com>
2021-12-03 08:05:18 +0200

    create permission class that check instance privacy and if user is has access to or not

--- core/permissions.py
+++ core/permissions.py
@@ -1,5 +1,7 @@
 from rest_framework import permissions
+from rest_framework.exceptions import APIException
 from rest_framework.permissions import SAFE_METHODS
+from rest_framework.status import HTTP_403_FORBIDDEN
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
@@ -10,3 +12,33 @@ class IsAdminOrReadOnly(permissions.BasePermission):
     IsJustRead = request.method in SAFE_METHODS
 
     return IsAdmin or IsJustRead
+
+class IsUserHasAccess:
+  message = 'You can\'t access private content.'
+  code = HTTP_403_FORBIDDEN
+
+  def __init__(self, request, instance, raise_exception=True) -> None:
+    self.request = request
+    self.instance = instance
+    self.raise_exception = raise_exception
+
+    return self.check()
+
+  def is_instance_private(self) -> bool:
+    return self.instance.is_private
+  
+  def is_user_has_access(self) -> bool:
+    user = self.request.user
+    return user.is_authenticated and (user.is_premium or user.is_stuff)
+  
+  def check(self):
+    state = True
+
+    if self.is_instance_private() and not self.is_user_has_access():
+      state = False
+
+      if self.raise_exception:
+        raise APIException(detail=self.message, code=self.code)
+
+    return state
+
