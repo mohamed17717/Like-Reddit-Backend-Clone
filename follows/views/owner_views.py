2ae73167f226e9fc663f5880cf04f6d285ede5b6
Mo17 <mo.err17@gmail.com>
2021-12-07 06:20:57 +0200

    make lists support paginations

--- follows/views/owner_views.py
+++ follows/views/owner_views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth import get_user_model
 
 from rest_framework import views
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 
 from accounts.serializers import UserBasicPublicSerializer
 
@@ -11,24 +11,29 @@ User = get_user_model()
 
 
 
-class UserFollow_ListFollows_ApiView(views.APIView):
+class UserFollow_ListFollows_ApiView(views.APIView, LimitOffsetPagination):
   ''' People he has follows '''
   permission_classes = [IsAuthenticated]
 
   def get(self, request, **kwargs):
     qs = User.objects.filter(user_followers__follower=request.user)
-    serialized = UserBasicPublicSerializer(qs, many=True)
 
-    return Response(serialized.data)
+    results = self.paginate_queryset(qs, request, view=self)
+    serializer = UserBasicPublicSerializer(results, many=True)
 
-class UserFollow_ListFollowers_ApiView(views.APIView):
+    return self.get_paginated_response(data=serializer.data)
+
+
+class UserFollow_ListFollowers_ApiView(views.APIView, LimitOffsetPagination):
   ''' People follows him '''
   permission_classes = [IsAuthenticated]
 
   def get(self, request, **kwargs):
     qs = User.objects.filter(user_targets__target=request.user)
-    serialized = UserBasicPublicSerializer(qs, many=True)
 
-    return Response(serialized.data)
+    results = self.paginate_queryset(qs, request, view=self)
+    serializer = UserBasicPublicSerializer(results, many=True)
+
+    return self.get_paginated_response(data=serializer.data)
 
 
