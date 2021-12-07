b99232d637f119a38fba9005434d25ac9d7b4abd
Mo17 <mo.err17@gmail.com>
2021-12-07 07:13:53 +0200

    modify serializers by creating new ones and show more fields and delete not necessery ones --- move some logic from views to the serializers

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,9 +1,11 @@
+from django.urls import reverse
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from djoser.serializers import UserCreateSerializer
 
-from accounts.models import UserProfile, UserVerified, UserPremium, UserBan
+from accounts.models import UserVerified, UserPremium, UserBan
+from follows.models import UserFollow
 
 
 User = get_user_model()
@@ -16,7 +18,6 @@ class UserCreateSerializer(UserCreateSerializer):
     REQUIRED_FIELDS = ['username']
 
 
-
 class UserBasicPublicSerializer(serializers.ModelSerializer):
   profile_picture = serializers.StringRelatedField(source='profile.profile_picture.url')
   profile_url = serializers.StringRelatedField(source='profile.get_absolute_url')
@@ -25,21 +26,72 @@ class UserBasicPublicSerializer(serializers.ModelSerializer):
     fields = ('username', 'first_name', 'last_name', 'email', 'profile_picture', 'profile_url')
 
 
+class UserProfilePageSerializer(UserBasicPublicSerializer):
+  is_followed = serializers.SerializerMethodField(read_only=True)
+  is_own_profile = serializers.SerializerMethodField(read_only=True)
+  followers_count = serializers.IntegerField(source='user_followers.count')
+  following_count = serializers.IntegerField(source='user_targets.count')
+  following_list_url = serializers.SerializerMethodField(read_only=True)
+  followers_list_url = serializers.SerializerMethodField(read_only=True)
+
+  class Meta(UserBasicPublicSerializer.Meta):
+    fields = UserBasicPublicSerializer.Meta.fields + (
+      'is_followed', 'is_own_profile', 'following_count', 'followers_count',
+      'following_list_url', 'followers_list_url'
+    )
+
+  def get_is_followed(self, obj):
+    request = self.context['request']
+
+    follow_state = False
+    if request.user.is_authenticated:
+      follow_state = UserFollow.objects.filter(target=obj, follower=request.user).exists()
+
+    return follow_state
+
+  def get_is_own_profile(self, obj):
+    request = self.context['request']
+    return request.user.is_authenticated and request.user.pk == obj.pk
+
+  def get_following_list_url(self, obj):
+    return reverse('follows:user-following-list', kwargs={'username': obj.username})
+
+  def get_followers_list_url(self, obj):
+    return reverse('follows:user-followers-list', kwargs={'username': obj.username})
+
 
 class UserVerifiedSerializer(serializers.ModelSerializer):
+  user = UserBasicPublicSerializer(read_only=True)
+  delete_url = serializers.SerializerMethodField()
+
   class Meta:
     model = UserVerified
-    fields = '__all__'
+    fields = ('user', 'delete_url')
+
+  def get_delete_url(self, obj):
+    return reverse('accounts:user-verify-detail', kwargs={'pk': obj.pk})
 
 
 class UserPremiumSerializer(serializers.ModelSerializer):
+  user = UserBasicPublicSerializer(read_only=True)
+  delete_url = serializers.SerializerMethodField()
+
   class Meta:
     model = UserPremium
-    fields = '__all__'
+    fields = ('user', 'delete_url')
 
+  def get_delete_url(self, obj):
+    return reverse('accounts:user-premium-detail', kwargs={'pk': obj.pk})
 
 class UserBanSerializer(serializers.ModelSerializer):
+  user = UserBasicPublicSerializer(read_only=True)
+  delete_url = serializers.SerializerMethodField()
+
   class Meta:
     model = UserBan
-    fields = '__all__'
+    fields = ('user', 'delete_url')
+
+  def get_delete_url(self, obj):
+    return reverse('accounts:user-ban-detail', kwargs={'pk': obj.pk})
+
 
--- accounts/views/anon_views.py
+++ accounts/views/anon_views.py
@@ -5,8 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 
-from accounts.serializers import UserBasicPublicSerializer
-from follows.models import UserFollow
+from accounts.serializers import UserProfilePageSerializer
 
 
 User = get_user_model()
@@ -16,13 +15,8 @@ class UserProfile_ApiView(APIView):
 
   def get(self, request, username):
     user = get_object_or_404(User, username=username)
-    serialized = UserBasicPublicSerializer(user)
+    serialized = UserProfilePageSerializer(user, context={'request': request})
 
-    check_follow = False
-    if request.user.is_authenticated:
-      check_follow = UserFollow.objects.filter(target=user, follower=request.user).exists()
-
-    data = {**serialized.data, 'is_followed': check_follow}
-    return Response(data)
+    return Response(serialized.data)
 
 
