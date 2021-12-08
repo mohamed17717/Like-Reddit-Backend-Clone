ea19d0b031ab2e4369c130173882985f7a012571
Mo17 <mo.err17@gmail.com>
2021-12-08 09:10:20 +0200

    add pagination featue to views and permissions

--- saves/views/owner_views.py
+++ saves/views/owner_views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404
 
-from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from core.generics import ToggleRecordGenericView
@@ -12,17 +13,19 @@ from saves.serializers import SavePostSerializer
 
 class SavePost_ToggleSave_ApiView(ToggleRecordGenericView):
   model = SavePost
+  permission_classes = [IsAuthenticated]
 
   def get_queryset_kwargs(self, request, post_id):
     post = get_object_or_404(Post, id=post_id)
     return {'user': request.user, 'post': post}
 
 
-class SavePost_ListSaves_ApiView(APIView):
-  def get(self, request, **kwargs):
-    qs = SavePost.objects.filter(user=request.user, post__existing_state='active')
-    serialized = SavePostSerializer(qs, many=True)
+class SavePost_ListSaves_ApiView(ListAPIView):
+  serializer_class = SavePostSerializer
+  permission_classes = [IsAuthenticated]
 
-    return Response(serialized.data)
+  def get_queryset(self):
+    qs = SavePost.objects.filter(user=self.request.user, post__existing_state='active')
+    return qs
 
 
