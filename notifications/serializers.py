b7cd736d01eeeb618c7aa18c3aebfa7ca9cd6026
Mo17 <mo.err17@gmail.com>
2021-12-08 08:48:52 +0200

    remove unused serializers

--- notifications/serializers.py
+++ notifications/serializers.py
@@ -1,17 +1,6 @@
 from rest_framework import serializers
-from notifications.models import NotificationType, NotificationMessage, Notification, NotificationSender
 
-
-class NotificationTypeSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = NotificationType
-    fields = '__all__'
-
-
-class NotificationMessageSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = NotificationMessage
-    fields = '__all__'
+from notifications.models import Notification
 
 
 class NotificationSerializer(serializers.ModelSerializer):
@@ -20,10 +9,4 @@ class NotificationSerializer(serializers.ModelSerializer):
   class Meta:
     model = Notification
     fields = ('msg', 'created', 'is_viewed', 'type', )
-
-
-class NotificationSenderSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = NotificationSender
-    fields = '__all__'
 
