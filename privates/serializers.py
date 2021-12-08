66a9034f0a4ecd3de5301492b776637c56a53a71
Mo17 <mo.err17@gmail.com>
2021-12-08 09:01:45 +0200

    remove serializers -- and abstract some views

--- privates/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from privates.models import PrivateContent
-
-
-class PrivateContentSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = PrivateContent
-    fields = '__all__'
-
--- privates/views/admin_views.py
+++ privates/views/admin_views.py
@@ -9,27 +9,26 @@ from privates.models import PrivateContent
 from threads.models import Thread
 from categories.models import Category, SubCategory
 
-class PrivateContent_ToggleThreadPrivate_ApiView(ToggleRecordGenericView):
+
+class Abstract_PrivateContent_ApiView(ToggleRecordGenericView):
   model = PrivateContent
   permission_classes = [IsAdminUser]
 
-  def get_queryset_kwargs(self, request, thread_id):
-    thread = get_object_or_404(Thread, id=thread_id)
+  related_model = None # required
+  lookup_url_kwarg = None # required
+
+  def get_queryset_kwargs(self, request, **kwargs):
+    item_id = kwargs.get(self.lookup_url_kwarg)
+    thread = get_object_or_404(self.related_model, id=item_id)
     return {'content_type': ContentType.objects.get_for_model(thread), 'content_id': thread.id}
 
+
+class PrivateContent_ToggleThreadPrivate_ApiView(Abstract_PrivateContent_ApiView):
+  related_model = Thread
+  lookup_url_kwarg = 'thread_id'
 class PrivateContent_ToggleCategoryPrivate_ApiView(ToggleRecordGenericView):
-  model = PrivateContent
-  permission_classes = [IsAdminUser]
-
-  def get_queryset_kwargs(self, request, category_id):
-    category = get_object_or_404(Category, id=category_id)
-    return {'content_type': ContentType.objects.get_for_model(category), 'content_id': category.id}
-
+  related_model = Category
+  lookup_url_kwarg = 'category_id'
 class PrivateContent_ToggleSubCategoryPrivate_ApiView(ToggleRecordGenericView):
-  model = PrivateContent
-  permission_classes = [IsAdminUser]
-
-  def get_queryset_kwargs(self, request, sub_category_id):
-    sub_category = get_object_or_404(SubCategory, id=sub_category_id)
-    return {'content_type': ContentType.objects.get_for_model(sub_category), 'content_id': sub_category.id}
-
+  related_model = SubCategory
+  lookup_url_kwarg = 'sub_category_id'
