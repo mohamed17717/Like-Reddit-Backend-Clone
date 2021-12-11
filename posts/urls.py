1937e148133cc9f2d58747b6f0dcae83c0c522a9
Mo17 <mo.err17@gmail.com>
2021-12-11 02:51:30 +0200

    remove admin views because it doesnt make sense to update state to soft_delete --- it must happen when delete by default not an endpoint

--- posts/urls.py
+++ posts/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 from rest_framework import routers
 
-from posts.views.admin_views import Post_AdminUpdateState_ApiView
 from posts.views.owner_views import Post_OwnerActions_ApiView
 from posts.views.user_views import Post_CreateReplay_ApiView
 from posts.views.anon_views import PostReplay_ListPostReplays_ApiView
@@ -18,5 +17,4 @@ urlpatterns = [
   path('post/<int:post_id>/replay/', Post_CreateReplay_ApiView.as_view(), name='create-post-replay'),
   path('post/<int:post_id>/replays/', PostReplay_ListPostReplays_ApiView.as_view(), name='list-post-replays'),
 
-  path('admin-post/<int:post_id>/update-state/', Post_AdminUpdateState_ApiView.as_view(), name='post-update-state'),
 ] + router.urls
