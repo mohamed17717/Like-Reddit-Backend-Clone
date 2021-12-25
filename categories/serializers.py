6c038d2c8a9b8eaddde9419c0e81a2456030cb06
Mo17 <mo.err17@gmail.com>
2021-12-26 00:14:16 +0200

    update serilaizer fields with readonly --- solve issue of creating relational field

--- categories/serializers.py
+++ categories/serializers.py
@@ -3,19 +3,20 @@ from categories.models import Category, SubCategory
 
 
 class CategoryBasicSerializer(serializers.ModelSerializer):
-  url = serializers.URLField(source='get_absolute_url')
+  url = serializers.URLField(source='get_absolute_url', read_only=True)
   class Meta:
     model = Category
     fields = ('name', 'is_private', 'url')
 
 class SubCategoryBasicSerializer(serializers.ModelSerializer):
-  url = serializers.URLField(source='get_absolute_url')
+  url = serializers.URLField(source='get_absolute_url', read_only=True)
   class Meta:
     model = SubCategory
     fields = ('name',  'is_private', 'url')
 
 class SubCategory_PlusParent_Serializer(SubCategoryBasicSerializer):
   category = CategoryBasicSerializer(read_only=True)
+  category_id = serializers.IntegerField(write_only=True)
   class Meta(SubCategoryBasicSerializer.Meta):
-    fields = SubCategoryBasicSerializer.Meta.fields + ('category',)
+    fields = SubCategoryBasicSerializer.Meta.fields + ('category', 'category_id')
 
