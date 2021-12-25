7507ebebf0b8565059119d15ff4ac8043f5112eb
Mo17 <mo.err17@gmail.com>
2021-12-26 00:45:58 +0200

    update some paths

--- follows/urls.py
+++ follows/urls.py
@@ -15,14 +15,14 @@ from follows.views.anon_views import (
 app_name = 'follows'
 
 urlpatterns = [
-  path('user/follow/<str:username>/', UserFollow_ToggleFollow_ApiView.as_view(), name="user-follow-toggle"),
-  path('user/follow/<str:username>/check/', UserFollow_CheckFollow_ApiView.as_view(), name="user-follow-check"),
+  path('follow/<str:username>/', UserFollow_ToggleFollow_ApiView.as_view(), name="user-follow-toggle"),
+  path('follow/<str:username>/check/', UserFollow_CheckFollow_ApiView.as_view(), name="user-follow-check"),
 
-  path('user/following/', UserFollowingList_ApiView.as_view(), name="current-user-following-list"),
-  path('user/followers/', UserFollowersList_ApiView.as_view(), name="current-user-followers-list"),
+  path('following/', UserFollowingList_ApiView.as_view(), name="current-user-following-list"),
+  path('followers/', UserFollowersList_ApiView.as_view(), name="current-user-followers-list"),
 
-  path('user/<str:username>/following/', UserFollowingList_ApiView.as_view(), name="user-following-list"),
-  path('user/<str:username>/followers/', UserFollowersList_ApiView.as_view(), name="user-followers-list"),
+  path('<str:username>/following/', UserFollowingList_ApiView.as_view(), name="user-following-list"),
+  path('<str:username>/followers/', UserFollowersList_ApiView.as_view(), name="user-followers-list"),
 
-  path('thread/follow/<int:thread_id>/', ThreadFollow_ToggleFollow_ApiView.as_view(), name="thread-follow-toggle"),
+  path('follow/thread/<int:thread_id>/', ThreadFollow_ToggleFollow_ApiView.as_view(), name="thread-follow-toggle"),
 ]
