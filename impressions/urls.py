5ef7f897ff4508292024122407b721767deb0ea5
Mo17 <mo.err17@gmail.com>
2021-12-26 21:05:32 +0200

    update urls

--- impressions/urls.py
+++ impressions/urls.py
@@ -13,13 +13,13 @@ from impressions.views.user_views import (
 app_name = 'impressions'
 router = routers.SimpleRouter()
 
-router.register(r'admin-post/emoji', Emoji_ApiView)
+router.register(r'admin/post/emoji', Emoji_ApiView, basename='admin-post-emoji')
 
 urlpatterns = [
-  path('user/react/<int:post_id>/emoji/<int:emoji_id>/', PostEmoji_UserReact_ApiView.as_view(), name='user-react-emoji-to-post'),
-  path('user/react/<int:post_id>/up/', PostUpVote_UserReact_ApiView.as_view(), name='user-react-upvote-to-post'),
-  path('user/react/<int:post_id>/down/', PostDownVote_UserReact_ApiView.as_view(), name='user-react-downvote-to-post'),
-  
+  path('react/<int:post_id>/emoji/<int:emoji_id>/', PostEmoji_UserReact_ApiView.as_view(), name='user-react-emoji-to-post'),
+  path('react/<int:post_id>/up/', PostUpVote_UserReact_ApiView.as_view(), name='user-react-upvote-to-post'),
+  path('react/<int:post_id>/down/', PostDownVote_UserReact_ApiView.as_view(), name='user-react-downvote-to-post'),
+
   path('post/emojis/', Emoji_UserList_ApiView.as_view(), name='user-list-emoji'),
 
 ] + router.urls
