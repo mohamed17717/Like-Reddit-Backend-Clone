f473f2d188632e9489d67659de3b4691fad65450
Mo17 <mo.err17@gmail.com>
2021-12-26 23:21:23 +0200

    update urls

--- rewards/urls.py
+++ rewards/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 
-from rewards.views.admin_views import (
-  UserKarma_List_ApiView
-)
+from rewards.views.admin_views import UserKarma_List_ApiView
 
 app_name = 'rewards'
 
 urlpatterns = [
-  path('admin-user/list-on-karma/', UserKarma_List_ApiView.as_view(), name="list-users-on-karma"),
+  path('admin/karma/list/', UserKarma_List_ApiView.as_view(), name="list-users-on-karma"),
 ]
