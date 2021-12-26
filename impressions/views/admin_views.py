7ba7607048cd581bd6c6d8ddc2ee8d19406c55c4
Mo17 <mo.err17@gmail.com>
2021-12-26 20:33:40 +0200

    add update feature for the api

--- impressions/views/admin_views.py
+++ impressions/views/admin_views.py
@@ -1,11 +1,11 @@
 from rest_framework.permissions import IsAdminUser
 
-from core.generics import CreateDestroyListViewSet
+from core.generics import CreateUpdateDestroyListViewSet
 
 from impressions.models import Emoji
 from impressions.serializers import EmojiSerializer
 
-class Emoji_ApiView(CreateDestroyListViewSet):
+class Emoji_ApiView(CreateUpdateDestroyListViewSet):
   queryset = Emoji.objects.all()
   serializer_class = EmojiSerializer
   permission_classes = [IsAdminUser]
