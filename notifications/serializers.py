7c8629a6ecf2d166c1442f21c248a012ba390714
Mo17 <mo.err17@gmail.com>
2021-12-28 20:16:56 +0200

    change field type and name

--- notifications/serializers.py
+++ notifications/serializers.py
@@ -5,9 +5,9 @@ from notifications.models import Notification
 
 class NotificationSerializer(serializers.ModelSerializer):
   url = serializers.URLField(source='sender.sender_object.get_notification_url')
-  msg = serializers.URLField(source='sender.sender_object.get_notification_message')
+  message = serializers.CharField(source='sender.sender_object.get_notification_message')
 
   class Meta:
     model = Notification
-    fields = ('msg', 'created', 'is_viewed', 'url', )
+    fields = ('message', 'created', 'is_viewed', 'url', )
 
