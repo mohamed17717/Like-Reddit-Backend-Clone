d8baab3986dbda320eae2f4a1372195c803f4560
Mo17 <mo.err17@gmail.com>
2021-12-29 08:19:20 +0200

    update urls names

--- reports/urls.py
+++ reports/urls.py
@@ -21,19 +21,23 @@ from reports.views.admin_views import (
 app_name = 'reports'
 router = routers.SimpleRouter()
 
-router.register(r'admin-report/type', ReportType_ApiView)
-router.register(r'admin-report/sub-type', ReportSubType_ApiView)
+router.register(r'admin/report/type', ReportType_ApiView, basename='admin-report-type')
+router.register(r'admin/report/sub-type', ReportSubType_ApiView)
 
 
 urlpatterns = [
   path('report/type/list/', ReportType_UserList_ApiView.as_view(), name='list-reports-type'),
   path('report/sub-type/list/', ReportSubType_UserList_ApiView.as_view(), name='list-reports-sub-types'),
   path('report/<int:type_id>/sub-type/list/', ReportSubType_UserListOnType_ApiView.as_view(), name='list-reports-sub-types-by-category'),
-  path('report/post/<int:post_id>/', PostReport_UserReport_Apiview.as_view(), name='post-report'),
 
-  path('admin-report/list/', PostReport_ListReports_ApiView.as_view(), name='list-reports'),
-  path('admin-report/decision/list/', ReportDecision_ListDecision_ApiView.as_view(), name='list-report-decisions'),
-  path('admin-report/decision/<int:report_id>/', PostReport_UpdateDecision_Apiview.as_view(), name='update-report-decision'),
+  path('post/<int:post_id>/report/<int:type_id>/', PostReport_UserReport_Apiview.as_view(), name='post-report'),
+
+  path('admin/report/list/', PostReport_ListReports_ApiView.as_view(), name='list-reports'),
+  path('admin/report/decision/list/', ReportDecision_ListDecision_ApiView.as_view(), name='list-report-decisions'),
+  path('admin/report/<int:report_id>/decision/warn/', PostReport_UpdateDecision_Apiview.as_view(), name='update-report-decision-warn'),
+  path('admin/report/<int:report_id>/decision/dangerous/', PostReport_UpdateDecision_Apiview.as_view(), name='update-report-decision-dangerous'),
+  path('admin/report/<int:report_id>/decision/safe/', PostReport_UpdateDecision_Apiview.as_view(), name='update-report-decision-safe'),
+  path('admin/report/<int:report_id>/decision/pending/', PostReport_UpdateDecision_Apiview.as_view(), name='update-report-decision-pending'),
 
 ] + router.urls
 
